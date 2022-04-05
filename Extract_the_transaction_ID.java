b88b4d93b7099a36fdae0607d6828f2bed12c628
Chandan Kumar Mahanta <75965142+ChandanMohonto@users.noreply.github.com>
2022-04-05 14:14:01 +0600

    Update Extract_the_transaction_ID.java

--- Extract_the_transaction_ID.java
+++ Extract_the_transaction_ID.java
@@ -8,22 +8,24 @@
 
 public class Extract_the_transaction_ID {
     public static void main(String[] args) {
-        String colon = "Your trnx is successful. Trnx Id is: TXN123456";  //Splitting colon separated String in Java
-        String[] strings = colon.split(":",0);
 
-        for (String strs: strings) {
-            System.out.println(strs);
-        }
-        
-        
-        //         **********************   also you can find this method    ****************************
 
-//        String StringValue = "Your trnx is successful. Trnx Id is: TXN123456";    // define the regex parameter
-//        String regex = ":";    // split the string object
+        String StringValue = "Your trnx is successful. Trnx Id is: TXN123456";    // define the regex parameter
+        String regex = ":";    // split the string object
+
+        String[]output = StringValue.split(regex);
+
+        System.out.println("Trnx Id:"+output[1]);
+
+//         **********************   also you can find this method    ****************************
+
+//        String colon = "Your trnx is successful. Trnx Id is: TXN123456";  //Splitting colon separated String in Java
+//        String[] strings = colon.split(":",0);
 //
-//        String[]output = StringValue.split(regex);
-//
-//        System.out.println("Trnx Id:"+output[1]);
+//        for (String strs: strings) {
+//            System.out.println(strs);
+//        }
+
 
     }
 }
