3daf0c1b6345362aeb6f44402737d4e9bba4b7cb
Chandan Kumar Mahanta <75965142+ChandanMohonto@users.noreply.github.com>
2022-04-07 20:49:45 +0600

    Add files via upload

---
+++ Tasbih counter.java
@@ -0,0 +1,16 @@
+﻿import java.util.Scanner;
+public class Tasbih_cunter {
+    public static void main(String[] args) {
+        int counter = 1;
+        System.out.println("Press Enter key for count Tasbih ");
+        while (true) {
+            Scanner takeinput = new Scanner(System.in);
+            String enterkey = takeinput.nextLine();
+            if (enterkey.equals("0")) {
+                break;
+            } else if (enterkey.equals("")) {
+                System.out.print(counter++);
+            }
+        }
+    }
+}
